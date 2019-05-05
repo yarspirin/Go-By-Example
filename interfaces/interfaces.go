c24b2bd6ee51504c1fe5134098e570c5ea745e15
Yaroslav Spirin <yar.spirin@gmail.com>
2019-05-05 21:50:11 +0300

    Add interfaces

--- interfaces/interfaces.go
+++ interfaces/interfaces.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import "fmt"
 
+type Animal interface {
+	makeSound()
+}
+
+type Cat struct {
+}
+
+func (cat Cat) makeSound() {
+	fmt.Println("Meow")
+}
+
+type Dog struct {
+}
+
+func (dog Dog) makeSound() {
+	fmt.Println("Woof")
+}
+
+func main() {
+	dog := Dog{}
+	cat := Cat{}
+
+	dog.makeSound()
+	cat.makeSound()
 }
